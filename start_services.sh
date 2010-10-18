9a89a34744742cf18169a206ff8e3eb44a3382b0
Allan Espinosa <aespinosa@cs.uchicago.edu>
2010-10-18 13:22:22 -0500

    Offset to accomodate pads in cybershake

--- start_services.sh
+++ start_services.sh
@@ -2,8 +2,8 @@
 
 NUMBER=$1
 SERVICE='/home/aespinosa/swift/trunk/bin/coaster-service'
-COASTER_PORT=62000
-WORKER_PORT=61000
+COASTER_PORT=61999
+WORKER_PORT=60999
 
 for i in `seq 0 $((NUMBER - 1))`; do
   $SERVICE -port $((COASTER_PORT + i)) -localport $((WORKER_PORT + i)) &
