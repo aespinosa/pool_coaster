ac467fd4a27158165958970dbbc6e1481fd47ffd
Allan Espinosa <aespinosa@cs.uchicago.edu>
2010-10-26 12:36:28 -0500

    Logging support

--- start_services.sh
+++ start_services.sh
@@ -6,7 +6,8 @@ COASTER_PORT=61999
 WORKER_PORT=60999
 
 for i in `seq 0 $((NUMBER - 1))`; do
-  $SERVICE -port $((COASTER_PORT + i)) -localport $((WORKER_PORT + i)) &
+  $SERVICE -port $((COASTER_PORT + i)) -localport $((WORKER_PORT + i)) &> \
+      service-$i.log  &
 done
 
 wait
