ab3340ee7fbdb14a6368a50d90d0d2e93da8d0c9
Allan Espinosa <aespinosa@cs.uchicago.edu>
2010-12-08 02:22:49 -0600

    Fresh pull from cogkit svn

--- worker.pl
+++ worker.pl
@@ -14,11 +14,11 @@ use IO::Socket;
 use File::Basename;
 use File::Path;
 use File::Copy;
-#use Time::HiRes qw(time);
 use Cwd;
 use POSIX ":sys_wait_h";
 use strict;
 use warnings;
+eval "use Time::HiRes qw(time)";
 
 # Maintain a stack of job slot ids for auxiliary services:
 #   Each slot has a small integer id 0..n-1
@@ -88,7 +88,6 @@ use constant REPLYTIMEOUT => 180;
 use constant MAXFRAGS => 16;
 use constant MAX_RECONNECT_ATTEMPTS => 3;
 
-my $LASTRECV = 0;
 my $JOBS_RUNNING = 0;
 
 my $JOB_COUNT = 0;
@@ -123,7 +122,6 @@ sub wlog {
 my $URISTR=$ARGV[0];
 my $BLOCKID=$ARGV[1];
 my $LOGDIR=$ARGV[2];
-my $IDLETIMEOUT = ( $#ARGV <= 2 ) ? (4 * 60) : $ARGV[3];
 
 
 # REQUESTS holds a map of incoming requests
@@ -300,7 +298,7 @@ sub init() {
 }
 
 sub logsetup() {
-        my $schemes = join(", ", @SCHEME);
+    my $schemes = join(", ", @SCHEME);
 	my $hosts = join(", ", @HOSTNAME);
 	my $ports = join(", ", @PORT);
 	wlog DEBUG, "uri=$URISTR\n";
@@ -308,7 +306,6 @@ sub logsetup() {
 	wlog DEBUG, "host=$hosts\n";
 	wlog DEBUG, "port=$ports\n";
 	wlog DEBUG, "blockid=$BLOCKID\n";
-    wlog DEBUG, "idletimeout=$IDLETIMEOUT\n";
 }
 
 sub sendm {
@@ -553,7 +550,6 @@ sub process {
 		}
 	}
 	else {
-		$LASTRECV = time();
 		if (!exists($REQUESTS{$tag})) {
 			$REQUESTS{$tag} = [{"dataIn" => \&processRequest}, time()];
 			wlog DEBUG, "New request ($tag)\n";
@@ -619,24 +615,22 @@ sub checkTimeouts {
 	$LASTTIMEOUTCHECK = $time;
 	checkTimeouts2(\%REQUESTS);
 	checkTimeouts2(\%REPLIES);
-	if ($LASTRECV != 0) {
-		my $dif = $time - $LASTRECV;
-		wlog TRACE, "time: $time, lastrecv: $LASTRECV, dif: $dif\n";
-		if ($dif >= $IDLETIMEOUT && $JOBS_RUNNING == 0) {
-			wlog INFO, "Idle time exceeded (time=$time, LASTRECV=$LASTRECV, dif=$dif)\n";
-			wlog INFO, "Ran a total of $JOB_COUNT jobs\n";
-			exit 0
-		}
-	}
 }
 
+my $DATA = "";
+
 sub recvOne {
-	my $data;
+	my $buf;
 	$SOCK->blocking(0);
-	$SOCK->recv($data, 12);
-	if (length($data) > 0) {
-		# wlog DEBUG, "Received " . unpackData($data) . "\n";
-		eval { process(unpackData($data)); } || (wlog ERROR, "Failed to process data: $@\n" && die "Failed to process data: $@");
+	$SOCK->recv($buf, 12 - length($DATA));
+	if (length($buf) > 0) {
+		$DATA = $DATA . $buf;
+		if (length($DATA) == 12) {
+			# wlog DEBUG, "Received " . unpackData($DATA) . "\n";
+			eval { process(unpackData($DATA)); } || (wlog ERROR, "Failed to process data: $@\n" && die "Failed to process data: $@");
+			$DATA = "";
+			return;
+		}
 	}
 	else {
 		#sleep 1ms
@@ -780,14 +774,16 @@ sub workershellcmd {
 		sendReply($tag, ("OK", "$out"));
 	}
 	else {
+		wlog DEBUG, "workershellcmd: $cmd\n";
 		$out = `$cmd 2>&1`;
+		wlog TRACE, "result: $out\n";
 		sendReply($tag, ("OK", "$out"));
 	}
 }
 
 sub urisplit {
 	my ($name) = @_;
-	
+
 	# accepted forms:
 	#   <protocol>://<host>/<path>
 	#   <protocol>:<path>
@@ -822,8 +818,8 @@ sub getFileCB {
 
 	wlog DEBUG, "getFileCB($jobid, $src, $dst)\n";
 
+	$src =~ s/pinned://;
 	my ($protocol, $path) = urisplit($src);
-	$protocol =~ s/pinned://;
 
 	wlog DEBUG, "$jobid src: $src, protocol: $protocol, path: $path\n";
 
@@ -884,6 +880,7 @@ sub getFileCBDataIn {
 	my $jobid = $$state{"jobid"};
 	wlog DEBUG, "$jobid getFileCBDataIn jobid: $jobid, state: $s, tag: $tag, err: $err, fin: $fin\n";
 	if ($err) {
+		wlog DEBUG, "$jobid getFileCBDataIn FAILED 520 Error staging in file: $reply\n";
 		queueCmd((nullCB(), "JOBSTATUS", $jobid, FAILED, "520", "Error staging in file: $reply"));
 		delete($JOBDATA{$jobid});
 		return;
@@ -964,7 +961,11 @@ sub stagein {
 		wlog DEBUG, "$jobid Staging in $$STAGE[$STAGEINDEX]\n";
 		$JOBDATA{$jobid}{"stageindex"} =  $STAGEINDEX + 1;
 		my ($protocol, $host, $path) = urisplit($$STAGE[$STAGEINDEX]);
-		if ($protocol eq "sfs") {
+		wlog DEBUG, "$jobid protocol: $protocol\n";
+		if ($$STAGE[$STAGEINDEX] =~ "pinned:.*") {
+			getPinnedFile($jobid, $$STAGE[$STAGEINDEX], $$STAGED[$STAGEINDEX]);
+		}
+		elsif ($protocol eq "sfs") {
 			my $dst = $$STAGED[$STAGEINDEX];
 			mkfdir($jobid, $dst);
 			if (!copy($path, $dst)) {
@@ -974,9 +975,6 @@ sub stagein {
 			else {
 				stagein($jobid);
 			}
-		}
-		elsif ($protocol =~ "pinned:") {
-			getPinnedFile($jobid, $$STAGE[$STAGEINDEX], $$STAGED[$STAGEINDEX]);
 		}
 		else {
 			getFile($jobid, $$STAGE[$STAGEINDEX], $$STAGED[$STAGEINDEX]);
@@ -1022,7 +1020,7 @@ sub getPinnedFile() {
 	}
 }
 
-sub mkPinnedDirectory() { 
+sub mkPinnedDirectory() {
 	my ($pinned_dir) = @_;
 	if (! $PINNED_READY) {
 		if (! -d $pinned_dir) {
@@ -1035,7 +1033,7 @@ sub mkPinnedDirectory() {
 	}
 }
 
-sub downloadPinnedFile() { 
+sub downloadPinnedFile() {
 	my ($jobid, $src, $dst, $rdst, $pinned_dir) = @_;
 	$PINNED{$rdst} = INFLIGHT;
 	getFile($jobid, $src, $dst);
@@ -1052,7 +1050,7 @@ sub linkToPinnedFile() {
 	my $dir = dirname($dst);
 	if (! -d $dir) {
 		wlog DEBUG, "mkpath: $dir\n";
-		mkpath($dir) || 
+		mkpath($dir) ||
 			die "getPinnedFile(): Could not mkdir: $dir\n";
 	}
 	link("$pinned_dir$rdst", $dst) ||
@@ -1388,7 +1386,6 @@ sub forkjob {
 	else {
 		queueCmd(nullCB(), "JOBSTATUS", $JOBID, FAILED, "512", "Could not fork child process");
 	}
-	$LASTRECV = time();
 }
 
 my $LASTJOBCHECK = 0;
