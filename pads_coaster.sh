91b04e1e332a6964146acf077f116f0c654e4a6e
Allan Espinosa <aespinosa@cs.uchicago.edu>
2010-12-08 02:23:11 -0600

    turned off notifications

--- pads_coaster.sh
+++ pads_coaster.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #PBS -q short
-#PBS -l walltime=02:00:00
+#PBS -l walltime=04:00:00
 #PBS -A CI-CCR000013
+#PBS -m n
 
 /home/aespinosa/swift/cogkit/modules/provider-coaster/resources/worker.pl \
-    http://communicado.ci.uchicago.edu:60999 FOOblock /home/aespinosa/tmp 7202
+    http://communicado.ci.uchicago.edu:60999 PADS /home/aespinosa/tmp 14402
